c0d0dd7306b927c2a3e38267a99de52af4e80990
dupput <dupput@gmail.com>
2021-05-15 22:58:34 +0100

    good setup

--- active.py
+++ active.py
@@ -1,114 +1,19 @@
 from scipy.optimize import fsolve
+import scipy as sci
 import vector_functions as vt
 import Stats_functions as st
 import sympy as sym
 import math
 import numpy as np
 from numpy import linalg as la
-from scipy import integrate
-from sympy import cos, sin, pi
+import re
+from sympy import cos, sin, pi, sympify, exp
 from sympy.vector import ParametricRegion, vector_integrate
-
-x, y, z, i, j, k, t, u, v = sym.symbols('x y z i j k t u v')
-
-
-vector = [y - z + 2, y*z + 4, -x*z]
-
-print(vt.curl(vector))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+import matplotlib.pyplot as plt
+import partial_differentiation_functions as pdf
+
+x, y, z, i, j, k, t, u, v, r, a, b, c, n, L, w, T, s \
+    = sym.symbols('x y z i j k t u v r a b c n L w T s')
 
 
 
